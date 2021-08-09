eb5f6fd12e78552a1369cc396d59da4b02380f6a
Lim Chee Keat <cheekeat@mailer.blueoceanit.com.my>
2021-08-10 06:50:49 +0800

    fix gitignore, setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,16 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-load_dotenv()
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'wf=)q+k67v)2dt*@n)aw+c+!ego)g(ny_29o!9xk^=)=eia(0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*',
+    'localhost',
+    'ck-website-backend.herokuapp.com',
 ]
 
 
