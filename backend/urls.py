27bf093c5fdf02fa4c634ba6fb61869c7362b399
Lim Chee Keat <cheekeat@mailer.blueoceanit.com.my>
2021-08-10 06:54:26 +0800

    fix setting.py

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.views import static
 from rest_framework import routers
 from task_tracker import views
 from django.conf.urls.static import static
